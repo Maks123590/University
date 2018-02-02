36e4296aa3dcd7f4cf40cc584442e106b37d644a
Maks123590 <33863319+Maks123590@users.noreply.github.com>
2018-02-02 20:47:23 +0300

    NWPL lab2 complete

--- Network and Web Programming languages/lab2/script.js
+++ Network and Web Programming languages/lab2/script.js
@@ -1,39 +1,90 @@
 (function() {
-
-    "use strict";
-
-    let countInput = document.querySelector(".count");
-    let div = document.querySelector(".arr");
-
-    let button = document.querySelector(".btn");
-
-    button.onclick = () => {
-
-        let different = 1;
-        let doubleCount = 0;
-
-        let count = countInput.value;
-
-        let arr = [];
-
-        arr[0] = prompt("input a[" + 0 + "]");
-
-        div.innerHTML += " " + arr[0];
-
-        for(let i = 1; i < count; i++) {
-            arr[i] = prompt("input a[" + i + "]");
     
-            if (arr[i] === arr[i-1]) {
-                doubleCount++;
-            } else {
-                different++;
-            }
+    let matr = [[1.0, 0.42, 0.54, 0.66],[0.42, 1.0, 0.32, 0.44],[0.54, 0.32, 1.0, 0.22],[0.66, 0.44, 0.22, 1.0]];
 
-            div.innerHTML += " " + arr[i];
-        }
-    
-        div.innerHTML += "<br/>" + "double count: " + doubleCount + " different count: " + different;
+    let detmatr = getMatrDet(matr);
 
+    if (detmatr === 1 || detmatr === -1) {
+        alert('Является ортогональной');
+    } 
+    else 
+    {
+        alert('Не является ортогональной');
     }
 
+
+    function getMatrDet(matrix) 
+    {
+        let n = matrix.length;
+        let m = matrix[0].length;
+        let detMatr = 0;
+
+        if (n === 1 && m === 1)
+        {
+            return matrix[0][0];
+        }
+
+        if (MatrDimIs2(matrix)) 
+        {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        } 
+        
+
+        let subMatr = [];
+
+        for(let i = 0; i < n; i++) {
+
+            subMatr = getSubmatrix(matrix, 0, i);
+
+            let sign = (i%2==0) ? 1 : -1;
+            detMatr += sign * matrix[0][i] * getMatrDet(subMatr);
+
+        }
+
+        return detMatr;
+    }
+
+    function MatrDimIs2(matrix) {
+        if (matrix.length !== 2) 
+        {
+            return false; 
+        } 
+        else 
+        {
+            if (matrix[0].length !== 2) 
+            {
+                return false;
+            } 
+            else 
+            {
+                return true;
+            }
+        }
+    }
+
+    function getSubmatrix(matrix, elemRowPosition, elemColumnPosition) 
+    {
+        let subMatr = [];
+
+        let tempRow = [];
+
+        for(let i = 0; i < matrix.length; i++)
+        {
+            if (i !== elemRowPosition)
+            {
+                for(j = 0; j < matrix[0].length; j++)
+                {
+                    if (j !== elemColumnPosition) {
+                        tempRow.push(matrix[i][j]);
+                    }
+                }
+    
+                subMatr.push(tempRow);
+                
+                tempRow = [];
+            }
+        }
+
+        return subMatr;
+    }
 }());
