0dcc501c5e44b5c2d4de589104b58eca90b6853b
Maks123590 <33863319+Maks123590@users.noreply.github.com>
2019-04-05 01:12:44 +0300

    OOP Task 8 add form view model

---
+++ Object Oriented Programming/Object Oriented Programming/Task8/Models/Web/Enums/TariffType.cs
@@ -0,0 +1,16 @@
+﻿namespace Task8.Models.Web.Enums
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TariffType
+    {
+        [Display(Name = "базовый")]
+        Base = 1,
+        [Display(Name = "почасовой")]
+        Daily = 2,
+        [Display(Name = "суточный")]
+        PerHours = 4,
+        [Display(Name = "студенческий")]
+        Student = 8
+    }
+}
---
+++ Object Oriented Programming/Object Oriented Programming/Task8/Models/Web/ViewModels/CarSharingFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Task8.Models.Web
+{
+    using Task8.Models.Web.Enums;
+
+    public class CarSharingFormViewModel
+    {
+        public int ClientAge { get; set; }
+
+        public int DriveMins { get; set; }
+
+        public int DriveKm { get; set; }
+
+        public TariffType TariffType { get; set; }
+
+        public bool UseGps { get; set; }
+
+        public bool AddDriver { get; set; }
+    }
+}
