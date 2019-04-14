3528fa70a31a823a7ff82c1fe9b15901996fcf7d
Maks123590 <33863319+Maks123590@users.noreply.github.com>
2019-04-15 01:53:25 +0300

    Шифр перестановки

---
+++ Information Security Methods/LAB 1/LAB1/Шифр перестановки/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Шифр_перестановки
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите строку для шифрования: ");
+            string str = Console.ReadLine();
+
+            var transcription = new Transposition();
+
+            transcription.SetKey("3 2 1");
+
+            var encrypted = transcription.Encrypt(str);
+
+            Console.WriteLine("Шифруем текст: {0}", encrypted);
+            Console.WriteLine("Дешифруем текст: {0}", transcription.Decrypt(encrypted).Substring(0, str.Length));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Information Security Methods/LAB 1/LAB1/Шифр перестановки/Transposition.cs
@@ -0,0 +1,83 @@
+﻿namespace Шифр_перестановки
+{
+    using System;
+
+    public class Transposition
+    {
+        private int[] Key { get; set; } = null;
+
+        public void SetKey(int[] key)
+        {
+            this.Key = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                this.Key[i] = key[i];
+            }
+        }
+
+        public void SetKey(string[] key)
+        {
+            this.Key = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                this.Key[i] = Convert.ToInt32(key[i]);
+            }
+        }
+
+        public void SetKey(string key)
+        {
+            this.SetKey(key.Split(' '));
+        }
+
+        public string Encrypt(string input)
+        {
+            for (int i = 0; i < input.Length % this.Key.Length; i++)
+            {
+                input += input[i];
+            }
+
+            var result = string.Empty;
+
+            for (var i = 0; i < input.Length; i += this.Key.Length)
+            {
+                char[] transposition = new char[this.Key.Length];
+
+                for (int j = 0; j < this.Key.Length; j++)
+                {
+                    transposition[this.Key[j] - 1] = input[i + j];
+                }
+
+                for (int j = 0; j < this.Key.Length; j++)
+                {
+                    result += transposition[j];
+                }
+            }
+
+            return result;
+        }
+
+        public string Decrypt(string input)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < input.Length; i += this.Key.Length)
+            {
+                char[] transposition = new char[this.Key.Length];
+
+                for (int j = 0; j < this.Key.Length; j++)
+                {
+                    transposition[j] = input[i + this.Key[j] - 1];
+                }
+
+                for (int j = 0; j < this.Key.Length; j++)
+                {
+                    result += transposition[j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
